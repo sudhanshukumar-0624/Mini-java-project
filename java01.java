9a5e30656a14f816c9c60375bf1d7b6276fd896c
sudhanshukumar-0624 <sudhanshusdsm@gamil.com>
2026-03-25 22:07:54 +0530

    Upload java01.java

---
+++ java01.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class java01{
+    public static void main (String[] args){
+
+        Scanner scanner = new Scanner (System.in);
+
+        System.out.print("Enter the item name = ");
+        String item = scanner.nextLine();
+
+        System.out.print("Enter the price = ");
+        int price = scanner.nextInt();
+        
+        System.out.print("Enter the quantity = ");
+        int quantity = scanner.nextInt();
+        
+        int Total = price*quantity;
+
+        System.out.println("You have bought: " + item);
+
+        System.out.println("The total amount of the item is : "+ Total);
+
+        scanner.close();
+    }
+}
